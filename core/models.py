de3c2c310dafe4e5556bf64fc88e1d154ac7fd75
cristobal1111 <cgonv22@gmail.com>
2021-07-06 18:20:14 -0400

    actualizado

--- core/models.py
+++ core/models.py
@@ -12,16 +12,6 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nombreCategoria
 
-#Modelo para el Vehiculo
-
-class Vehiculo(models.Model):
-    patente = models.CharField(max_length=6,primary_key=True, verbose_name='Patente')
-    marca = models.CharField(max_length=20, verbose_name='Marca Vehiculo')
-    modelo = models.CharField(max_length=20,null=True, blank=True, verbose_name='Modelo')
-    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
-    def str(self):
-        return self.patente
-
 
 #modelO opara el cliente
 
