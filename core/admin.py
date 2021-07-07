11b161514a08fe9ae49933dc50d58f851dc9e24f
Christian2299 <ch.guerrerom@duocuc.cl>
2021-07-06 20:30:42 -0400

    CAMBIOS

--- core/admin.py
+++ core/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-from .models import Categoria, CategoriaUsuario, Vehiculo, Usuario
-
+from .models import Categoria, CategoriaUsuario, Usuario, Vehiculo 
 
 # Register your models here.
 #Permite administrar el modelo completo
 
 admin.site.register(Categoria)
-admin.site.register(Vehiculo)
 admin.site.register(Usuario)
+admin.site.register(Vehiculo)
 admin.site.register(CategoriaUsuario)
+
 
 
 
--- core/models.py
+++ core/models.py
@@ -12,6 +12,16 @@ class Categoria(models.Model):
     def __str__(self):
         return self.nombreCategoria
 
+#Modelo para el Vehiculo
+
+class Vehiculo(models.Model):
+    patente = models.CharField(max_length=6,primary_key=True, verbose_name='Patente')
+    marca = models.CharField(max_length=20, verbose_name='Marca Vehiculo')
+    modelo = models.CharField(max_length=20,null=True, blank=True, verbose_name='Modelo')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    def str(self):
+        return self.patente
+
 
 #modelO opara el cliente
 
@@ -24,7 +34,8 @@ class Usuario(models.Model):
     def str(self):
         return self.patente
     
-#Categoria para el usuario
+ 
+    #Categoria para el usuario
 
 class CategoriaUsuario(models.Model):
     iddCategoria = models.IntegerField(max_length=6,primary_key=True, verbose_name='id de Categoria')
@@ -32,4 +43,3 @@ class CategoriaUsuario(models.Model):
 
     def str(self):
         return self.NombredCategoria
-        
